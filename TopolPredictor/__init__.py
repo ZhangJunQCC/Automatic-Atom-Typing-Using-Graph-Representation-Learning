e49aaab7d9dee11567d9a21e555f8b2fd823a9bf
Jun Zhang <zhangjunqcc@gmail.com>
2022-03-04 16:04:37 +0800

    Delete __init__.py

--- TopolPredictor/__init__.py
+++
@@ -1,4 +0,0 @@
-from .TopolData import TopolData
-from .TopolNet import TopolConv, TopolNet, TopolTrain
-from .TopolUtility import TopolUtility
-from .TopolNetExplainer import TopolConvExplainer, TopolNetExplainer
